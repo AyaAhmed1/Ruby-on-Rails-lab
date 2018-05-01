34eb65c9790326a234128bef2db409126e7cf689
AyaAhmed1 <ayaahmed9424@gmail.com>
2018-05-01 14:58:16 +0200

    modifcation on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+    validates :name, presence:true 
+    enum gender: {male: 0,female: 1}
+
+
 end
+   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+  #resources :users ,only: [:index ,:edit]
+  #resources :users ,except: [:delete]
+  # match '/users' , 'users#index'
+  # match '/users/:id' , 'users#show'
+  # post  '/users' , 'users#create' , as :give_me_users_create
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
